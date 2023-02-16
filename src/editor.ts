7c7b9900547b1a8fe2c3bc6a6ef156caa89cce33
Martin Vladic <martin.vladic@gmail.com>
2023-02-16 14:18:25 +0100

    component runtime execute function moved to registerActionComponent

--- src/editor.ts
+++ src/editor.ts
@@ -3,7 +3,8 @@ import {
     IVariable,
     GenericDialogResult,
     IObjectVariableValueConstructorParams,
-    IActionComponent
+    IActionComponent,
+    IDashboardComponentContext
 } from "eez-studio-types";
 import pg from "pg";
 
@@ -70,6 +71,70 @@ export default {
                         }
                     ];
                 }
+            },
+
+            execute: (context: IDashboardComponentContext) => {
+                interface PostgreSQLConnectionVariableTypeConstructorParams {
+                    host: string;
+                    port: number;
+                    user: string;
+                    password: string;
+                    database: string;
+                    ssl: boolean;
+                }
+
+                const connection =
+                    context.evalProperty<PostgreSQLConnectionVariableTypeConstructorParams>(
+                        "connection"
+                    );
+
+                if (!connection) {
+                    context.throwError(`Invalid PostgreSQL connection`);
+                    return;
+                }
+
+                const sql = context.evalProperty<string>("sql");
+                if (!sql) {
+                    context.throwError(`Invalid SQL`);
+                    return;
+                }
+
+                if (context.logInfo) {
+                    context.logInfo(sql);
+                }
+
+                context = context.startAsyncExecution();
+
+                (async () => {
+                    const config: pg.ClientConfig = {
+                        host: connection.host,
+                        port: connection.port,
+                        user: connection.user,
+                        password: connection.password,
+                        database: connection.database
+                    };
+
+                    if (connection.ssl) {
+                        config.ssl = {
+                            rejectUnauthorized: false
+                        };
+                    }
+
+                    try {
+                        const client = new pg.Client(config);
+                        await client.connect();
+                        const res = await client.query(sql);
+                        await client.end();
+
+                        context.propagateValue("result", res?.rows ?? []);
+                        context.propagateValueThroughSeqout();
+                    } catch (err) {
+                        console.error(err);
+                        context.throwError(err.toString());
+                    }
+
+                    context.endAsyncExecution();
+                })();
             }
         });
 
